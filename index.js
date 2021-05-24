03e98fb8deacec7bbd0fd9b116c0142ce8fdcff3
Pranav Hegde <pranavhegde06@gmail.com>
2021-05-24 13:25:49 +0530

    add counter

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+const { count } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
 const url = require('url');
+let counter = 0
+
+
+app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
   }))
 
+app.use(
+  express.static('public', {
+    setHeaders: (res, path, stat) => {
+      ++counter;
+    },
+  })
+)
 app.use(express.static('public'));  
 app.use('/images', express.static('images')); 
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/templates/index.html'))
-})
-
+app.get('/', function (req, res) {
+  res.render('./../public/views/index', {count: parseInt(counter/3)});
+});
 
 app.use('/api/caesar', require('./server/api/Caesar'));
 
