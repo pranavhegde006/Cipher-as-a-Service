e56f02e420df12db31b6ec82693c6521577dc25f
Pranav Hegde <pranavhegde06@gmail.com>
2021-05-25 21:23:34 +0530

    add decryption

--- server/helper/cipherFunctions.js
+++ server/helper/cipherFunctions.js
@@ -10,12 +10,19 @@ const caesarCipher = (message, key) => {
     return cipherText;
 }
 
+const modulo = (number1, number2) => {
+    if(number1 < 0){
+        return number1%number2 + number2;
+    }
+    return number1%number2;
+}
+
 const caesarDecrypt = (message, key) => {
     let plainText = '', messageArr = [], messageStr = message.toLowerCase();
     for(var i = 0; i < message.length; i++){
         messageArr.push(messageStr[i].charCodeAt() - 'a'.charCodeAt());
         messageArr[i] -= parseInt(key);
-        messageArr[i] %= 26;
+        messageArr[i] = modulo(messageArr[i], 26);
         messageArr[i] += 97;
         plainText += String.fromCharCode(messageArr[i]);
     }
